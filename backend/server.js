6b00e02344d2a486f9ab5a32809fd81913958137
Sandeepbh07 <cheetrirocker07@gmail.com>
2022-07-30 09:47:43 +0545

    mvc added and joi validation added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 require("dotenv").config();
+
+//routes
+const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(morgan("combined"));
+
+//register the routes
+app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
 
