9d9cc1451f8b22617449ccd9d6ea356f60cc0984
Sandeepbh07 <cheetrirocker07@gmail.com>
2022-07-30 10:00:11 +0545

    register logic added

--- backend/controllers/auth/postRegister.js
+++ backend/controllers/auth/postRegister.js
@@ -1,5 +1,28 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../models/user");
+
 const postRegister = async (req, res) => {
-  res.send("register route");
+  try {
+    const { username, mail, password } = req.body;
+
+    //check if exists
+    const userExists = await User.exists({ mail });
+    if (userExists) {
+      return res.status(409).send("E-mail already in use.");
+    }
+
+    //encrypt password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    //create user document and save it in the database
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+  } catch (error) {
+    return res.status(500).send("Error occured. Please try again");
+  }
 };
 
 module.exports = postRegister;
